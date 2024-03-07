399cf06c57ab11782ba54d0dafd0ab65452943b2
Enrique Viña Alonso <enriquevialon2@gmail.com>
2024-03-07 15:42:36 +0000

    Added string validation for the path and name textboxes in  NewProjectViewModel

--- WackEditor/GameProject/NewProjectViewModel.cs
+++ WackEditor/GameProject/NewProjectViewModel.cs
@@ -36,7 +36,7 @@ namespace WackEditor.GameProject
     class NewProjectViewModel : ViewModelBase
     {
         // TODO: Get path from installation location.
-        private readonly string _templatePath = @"..\..\WackEditor\ProjectTemplates";
+        private readonly string _templatePath = @"..\..\WackEditor\ProjectTemplates\";
 
         private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\WackEngine\";
         private string _projectName = "NewProject";
@@ -49,6 +49,7 @@ namespace WackEditor.GameProject
                 if (_projectName != value)
                 {
                     _projectName = value;
+                    ValidateProjectPath();
                     OnPropertyChanged(nameof(ProjectName));
                 }
             }
@@ -61,13 +62,84 @@ namespace WackEditor.GameProject
                 if (_projectPath != value)
                 {
                     _projectPath = value;
+                    ValidateProjectPath();
                     OnPropertyChanged(nameof(ProjectPath));
                 }
             }
         }
 
+        //indicates wether the project path is valid
+        private bool _isValid;
+
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            }
+        }
+
+        private string _errorMsg;
+        public string ErrorMsg {
+            get => _errorMsg;
+            set
+            {
+                if (_errorMsg != value)
+                {
+                    _errorMsg = value;
+                    OnPropertyChanged(nameof(ErrorMsg));
+                }
+            }
+        }
+
+
         private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
         public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+
+        private bool ValidateProjectPath()
+        {
+            var path = ProjectPath;
+            if (!Path.EndsInDirectorySeparator(path))
+            {
+                path += @"\";
+            }
+            path += @"{ProjectName}\";
+
+            IsValid = false;
+
+            if(string.IsNullOrEmpty(ProjectName.Trim())) {
+                ErrorMsg = "Type in a project name.";
+            }
+            else if(ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                ErrorMsg = "Invalid character(s) used in project name.";
+            }
+            else if (string.IsNullOrWhiteSpace(ProjectPath.Trim()))
+            {
+                ErrorMsg = "Select a valid project folder";
+            }
+            else if(ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorMsg = "Invalid character(s) used in project path.";
+            }
+            else if(Directory.Exists(path) && Directory.EnumerateFileSystemEntries(path).Any())
+            {
+                ErrorMsg = "Target project folder already exists and its not empty.";
+            }
+            else
+            {
+                ErrorMsg = string.Empty;
+                IsValid = true;
+            }
+
+            return IsValid;
+        }
 
         public NewProjectViewModel()
         {
@@ -87,6 +159,7 @@ namespace WackEditor.GameProject
                     template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(filename), template.ProjectFile));
                     _projectTemplates.Add(template);
                 }
+                ValidateProjectPath();
 
             }
             catch (Exception ex)
